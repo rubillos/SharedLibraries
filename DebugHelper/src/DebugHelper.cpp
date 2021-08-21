c97b7c43b617d14842ba43a88aa3e114fcf5c3db
Randy Ubillos <randy@mac.com>
2021-08-21 13:22:20 -0700

    Support for RTT messaging.

--- DebugHelper/src/DebugHelper.cpp
+++ DebugHelper/src/DebugHelper.cpp
@@ -1,1 +1,12 @@
 #include "DebugHelper.h"
+
+#ifdef DO_RTT
+RTT_Helper rtt;
+#endif
+
+extern "C" char *sbrk(int i);
+
+int32_t FreeRam() {
+  char stack_dummy = 0;
+  return &stack_dummy - sbrk(0);
+}
--- DebugHelper/src/DebugHelper.h
+++ DebugHelper/src/DebugHelper.h
@@ -1,11 +1,13 @@
 #ifndef __DEBUG_HELPER__
 #define __DEBUG_HELPER__
 
+#include <stdint.h>
+
 #ifdef DO_SERIAL
 #define Debug_print(...) Serial.print(__VA_ARGS__)
 #define Debug_println(...) Serial.println(__VA_ARGS__)
 #define Debug_begin(...) Serial.begin(__VA_ARGS__)
-#define Debug_begin_wait(...) { Serial.begin(__VA_ARGS__); while (!Serial); Serial.println() delay(250); }
+#define Debug_begin_wait(...) { Serial.begin(__VA_ARGS__); while (!Serial); Serial.println(); delay(250); }
 #define Debug_delay(...) delay(__VA_ARGS__)
 #define Debug_defined
 #endif
@@ -14,10 +16,24 @@
 //  monitor_port = socket://localhost:19021
 
 #ifdef DO_RTT
-// #include "../lib/RTT/SEGGER_RTT.h"
 #include "SEGGER_RTT.h"
-#define Debug_print(...) SEGGER_RTT_printf(0, __VA_ARGS__)
-#define Debug_println(...)  SEGGER_RTT_printf(0, __VA_ARGS__); SEGGER_RTT_printf(0, "\n")
+#include "Print.h"
+
+class RTT_Helper : public Print {
+    virtual size_t write(uint8_t c) {
+        SEGGER_RTT_PutChar(0, c);
+        return 1;
+    };
+    virtual size_t write(const uint8_t *buffer, size_t size) {
+        SEGGER_RTT_Write(0, buffer, size);
+        return size;
+    };
+};
+
+extern RTT_Helper rtt;
+
+#define Debug_print(...) rtt.print(__VA_ARGS__)
+#define Debug_println(...) rtt.println(__VA_ARGS__)
 #define Debug_begin(...) SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL)
 #define Debug_begin_wait(...) Debug_begin(__VA_ARGS__)
 #define Debug_delay(...) delay(__VA_ARGS__)
@@ -31,6 +47,8 @@
 #define Debug_begin_wait(...)
 #define Debug_delay(...)
 #endif
+
+extern int32_t FreeRam();
 
 // Add this after Debug setup in setup()
 // initial_stop: NOT_USED;
