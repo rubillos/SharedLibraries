e02da9f5858027183efa5033be7676f094ecf942
Randy Ubillos <randy@mac.com>
2022-08-22 08:47:33 -0800

    Fixed compile warning. Added LoopBuff.

--- RingBuff/RingBuff.h
+++ RingBuff/RingBuff.h
@@ -25,7 +25,7 @@ class RingBuff {
       if (_sampleCount == 0) {
         return _buffer[0];
       }
-      int16_t destIndex = _index-1-min(itemIndex, _sampleCount-1);
+      int16_t destIndex = _index-1-min((uint16_t)itemIndex, (uint16_t)(_sampleCount-1));
       while (destIndex < 0) {
         destIndex += _bufferSize;
       }
@@ -38,6 +38,36 @@ class RingBuff {
     T* _buffer;
     T _sum;
 		T _zero;
+    uint16_t _bufferSize;
+    uint16_t _sampleCount;
+    uint16_t _index;
+};
+
+template <class T>
+class LoopBuff {
+  public:
+    LoopBuff(uint16_t bufferSize) { _buffer = new T[bufferSize](); _bufferSize = bufferSize; _index=0; _sampleCount=0; };
+
+    void addSample(T data) {
+      _buffer[_index] = data;
+      _index = (_index + 1) % _bufferSize;
+      if (_sampleCount < _bufferSize) _sampleCount++;
+    };
+    T lookup(uint16_t itemIndex) {
+      if (_sampleCount == 0) {
+        return _buffer[0];
+      }
+      int16_t destIndex = _index-1-min((uint16_t)itemIndex, (uint16_t)(_sampleCount-1));
+      while (destIndex < 0) {
+        destIndex += _bufferSize;
+      }
+      return _buffer[destIndex];
+    };
+    uint16_t sampleCount() { return _sampleCount; };
+    void reset() { _sampleCount = 0; }
+
+  private:
+    T* _buffer;
     uint16_t _bufferSize;
     uint16_t _sampleCount;
     uint16_t _index;
